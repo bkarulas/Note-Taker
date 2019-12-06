24d8a58eefab6ff74660dc5cfae43fac96b3d50c
Brad <bkarulas@outlook.com>
2019-12-06 18:58:55 -0500

    Get pages working and listening on port 3000

--- server.js
+++ server.js
@@ -2,9 +2,31 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = 3000;
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
+//GET SECTION
+//note page
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+//home page
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+//END OF GET SECTION
+
+//POST SECTION
+
+//END OF POST SECTION
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
