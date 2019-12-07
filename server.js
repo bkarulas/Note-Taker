4b965675738e3e813d34e7c85862bbd593b0455e
Brad <bkarulas@outlook.com>
2019-12-06 20:54:40 -0500

    Updated GET section

--- server.js
+++ server.js
@@ -11,22 +11,33 @@ var PORT = 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 //GET SECTION
+
 //note page
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+//get notes
+app.get("/api/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+  res.json(savedNotes[Number(req.params.id)]);
+});
+
 //home page
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 //END OF GET SECTION
 
-//POST SECTION
-
-//END OF POST SECTION
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log(`App listening on PORT ${PORT}`);
 });
